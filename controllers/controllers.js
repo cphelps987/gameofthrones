1916725e5ff86055e56bd23f362d72f4c5dc532e
cphelps987 <cphelps987@gmail.com>
2017-07-03 16:19:27 -0400

    adding files

---
+++ controllers/controllers.js
@@ -0,0 +1,6 @@
+/**
+ * Created by courtneyphelps on 7/3/17.
+ */
+var express = require("express");
+
+modal.import("../model/model.js");
---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by courtneyphelps on 7/3/17.
+ */
+var express = require ("express");
+var exphbs = require ("express-handlebars");
+var mysql = require ("mysql");
+var bp = require ("body-parser");
+
+var app = express();
+var port = 3000;
