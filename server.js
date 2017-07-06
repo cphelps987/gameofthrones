dff62dc4dbaf38f9e18b2b80d6955ccf475edf05
cphelps987 <cphelps987@gmail.com>
2017-07-05 21:05:04 -0400

    added packages in server file and basic handlebars in main html

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 /**
  * Created by courtneyphelps on 7/3/17.
  */
+//express and body parser
 var express = require ("express");
-var exphbs = require ("express-handlebars");
-var mysql = require ("mysql");
-var bp = require ("body-parser");
+var bodyParser = require ("body-parser");
 
 var app = express();
 var port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//express and handlebars
+var exphbs = require ("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine","handlebars");
+
+//mysql
+
+var mysql = require ("mysql");
+
+var connection = mysql.createConnection({
+    host    : "localhost",
+    user    : "root",
+    password: "",
+    database: "gameofthrones"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting " + err.stack);
+    }
+    console.log("connection as id " + connection.threadId);
+});
+
+app.listen(port, function () {
+   console.log("Listening on PORT " + port);
+});
+
