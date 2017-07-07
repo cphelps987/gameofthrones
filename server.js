5b1ca298850e0471e02f82aa5da28ac76097383e
cphelps987 <cphelps987@gmail.com>
2017-07-07 09:09:54 -0400

    changed port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const exphbs = require ("express-handlebars");
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false}));
